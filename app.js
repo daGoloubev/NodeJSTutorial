0398b37556ce59c9c32603e9f41a1aa4f12994ea
Daniel Goloubev <danielgoloubev@gmail.com>
2017-11-23 16:52:30 +0100

    Adds Request handling and how it works.

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000; 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; 
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const HTTP = require('http');
+const IP = '127.0.0.1';
+const PORT = 3000;
+HTTP.createServer((REQUEST, response) => {
+	const {headers, method, url} = REQUEST;
+	let body = [];
+	REQUEST.on('error', (err) => {
+		console.error(err.stack);
+	})
+	.on('data', (chunk) => {
+		body.push(chunk);
+	})
+	.on('end', () => {
+		body = Buffer.concat(body).toString();
+		// At this point, we have the headers, method, url and body, and can now
+		// do whatever we need to in order to repond to this request.
+	});
+}).listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+}); // Activates this server , listening on port 8080
