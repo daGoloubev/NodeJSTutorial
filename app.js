835bf4c9a9a3feefeac3c6e757ff58f1b1f8382c
Daniel Goloubev <danielgoloubev@gmail.com>
2017-11-24 07:00:47 +0100

    Response version 1

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const HTTP = require('http');
 const IP = '127.0.0.1';
 const PORT = 3000;
-HTTP.createServer((REQUEST, response) => {
-	const {headers, method, url} = REQUEST;
+HTTP.createServer((REQUEST, RESPONSE) => {
+	const { headers, method, url } = REQUEST;
 	let body = [];
 	REQUEST.on('error', (err) => {
+		// This prinst the error message and the stack trace to 'stderr'
 		console.error(err.stack);
 	})
 	.on('data', (chunk) => {
@@ -15,6 +16,20 @@ HTTP.createServer((REQUEST, response) => {
 		// At this point, we have the headers, method, url and body, and can now
 		// do whatever we need to in order to repond to this request.
 	});
+	/////////////////// RESPONSE ///////////////////////
+	RESPONSE.on('error', (err) => {
+		console.error(err.stack);
+	});
+	RESPONSE.statusCode = 200; 
+	RESPONSE.setHeader('Content-Type', 'application/json');
+	// Note: the 2 lines above could be replaced with this next one: 
+	// RESPONSE.writeHead(200, {'Content-Type': 'application/json'});
+	const RESPONSE_BODY = { headers, method, url, body };
+	RESPONSE.write(JSON.stringify(RESPONSE_BODY));
+	RESPONSE.end();
+	// Note: the 2 lines above could be replaced with this next one: 
+	// RESPONSE.END(JSON.stringify(RESPONSE_BODY));
+	
 }).listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 }); // Activates this server , listening on port 8080
