6b0a08682185b9c6e3cfef08cbe7b252b07214d9
Daniel Goloubev <danielgoloubev@gmail.com>
2017-11-24 07:32:29 +0100

    Streams and pipes

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const HTTP = require('http');
 const IP = '127.0.0.1';
 const PORT = 3000;
 HTTP.createServer((request, response) => {
+	request.on('error', (err) =>{
+		console.error(err);
+		response.statusCode = 400;
+		response.end();
+	});
+	response.on('error', (err) => {
+		console.error(err);
+	});
 	if(request.method === 'POST' && request.url === '/echo'){
-		let body = [];
-		request.on('data', (chunk) => {
-			body.push(chunk);
-		}).on('end', () =>{
-			body = Buffer.concat(body).toString();
-			response.end(body);
-		});	
+		request.pipe(response)	
 	} else {
 		response.statusCode = 404; 
 		response.end();
